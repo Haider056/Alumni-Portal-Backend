b3e171e451f325534b90c981c1fdf0aa1ea8e35b
Muhammad Haider <111388932+Haider056@users.noreply.github.com>
2024-08-14 02:48:52 +0500

    Update Server.js

--- Server.js
+++ Server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: 'http://192.168.10.41', 
+  origin: 'http://localhost:3000', 
   credentials: true,
 }));
 
